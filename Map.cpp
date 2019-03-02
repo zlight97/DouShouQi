4d0a9e51cb92a5d645f4fb52ffc974d38f5ac0e6
Randy Lam <rlam27@ucmerced.edu>
2019-03-02 00:12:45 -0800

    Thank Matt for debugs

--- Map.cpp
+++ Map.cpp
@@ -1,14 +1,24 @@
 //Mapp.cpp
 #include "Map.h"
 
-// Map::Map(float x, float y, float w, float h) {
-//     this x -> x;
-//     this y -> y;
-//     this w -> w;
-//     this h -> h;
-// }
+Map::Map(float x, float y, float w, float h) {
+    this -> x =  x;
+    this -> y =  y;
+    this -> w =  w;
+    this -> h = h;
 
-// Map::~Map() {
+    grassTex = new TextRect("Grass.png",x,y,w,h);
+}
+
+Map::Map(float x, float y, float w, float h, bool traversable) {
+    this -> x =  x;
+    this -> y =  y;
+    this -> w =  w;
+    this -> h = h;
+    this -> trav = traversable;
+    waterTex = new TextRect("Water.png",x,y,w,h,traversable);
+}
+Map::~Map() {
     // delete Map;
-    // Delete tex 
-// }
+    // delete text;
+}
--- Map.h
+++ Map.h
@@ -1,14 +1,16 @@
 //Map class
 #ifndef MAP_H
 #define MAP_H
+#include "TextRect.h"
 
 class Map {
     float x,y,w,h;
-    bool occupied = false;
-    // TexRect *grassTex;
-    // TexRect *waterTex;
+    bool trav = true;
+    TextRect *grassTex;
+    TextRect *waterTex;
     public:
     Map(float x,float y,float w,float h);
+    Map(float x,float y,float w,float h, bool trav);
     ~Map();
 };
 
